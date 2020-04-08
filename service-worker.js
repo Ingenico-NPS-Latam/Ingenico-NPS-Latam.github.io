acdad209e368b1ca8508a612e700bc9b09e0da05
Ramiro D'Accorso <ramiro.daccorso@epay.ingenico.com>
2020-04-08 17:58:20 -0300

    Updated to lastest version

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f8c0a793064ba35a098fdf13a58cf69b.js"
+  "/precache-manifest.bc09312e71663d2d2d217f5862932076.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
